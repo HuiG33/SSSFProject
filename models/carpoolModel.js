4bdc0419521d02a408e22e76472926c668d99300
HuiG33 <joni.tefke12@gmail.com>
2019-04-25 16:54:00 +0300

    coordinates to carpool

--- models/carpoolModel.js
+++ models/carpoolModel.js
@@ -6,21 +6,37 @@ const Schema = mongoose.Schema;
 const carpoolSchema = new Schema({
   start: {
     town: String,
-    address: String
+    address: String,
+    coordinates: {
+      lat: String,
+      lng: String
+    }
   },
   end: {
     town: String,
-    address: String
+    address: String,
+    coordinates: {
+      lat: String,
+      lng: String
+    }
   },
   waypoints: [{
     town: String,
     address: String,
-    hitchhiker: String
+    hitchhiker: String,
+    coordinates: {
+      lat: String,
+      lng: String
+    }
   }],
   suggestions: [{
     town: String,
     address: String,
-    hitchhiker: String
+    hitchhiker: String,
+    coordinates: {
+      lat: String,
+      lng: String
+    }
   }]
 });
 
